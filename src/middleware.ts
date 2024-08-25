1610f1764e4df4976113f81bdbd832a1302a3f9c
Nikhil2005Rai <n2005rai@gmail.com>
2024-08-25 12:25:28 +0530

    Add chech username endpoint

--- src/middleware.ts
+++ src/middleware.ts
@@ -24,7 +24,6 @@ export async function middleware(request: NextRequest) {
     return NextResponse.next()
 }
  
-// See "Matching Paths" below to learn more
 export const config = {
   matcher: [
         '/sign-in',
